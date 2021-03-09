6606c482ff935e04acafd924c880cd09402b8709
qpcio <qpcio@onet.eu>
2021-03-09 16:05:53 +0100

    +log4j xml

--- src/test/java/Day2/BaseTest.java
+++ src/test/java/Day2/BaseTest.java
@@ -2,17 +2,28 @@ package Day2;
 
 import io.cucumber.java.bs.A;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
+    Logger logger = Logger.getLogger(BaseTest.class);
+
+    @BeforeClass
+    public void setLogging(){
+        DOMConfigurator.configure("log4j.xml");
+    }
+
+
 
 
     @BeforeMethod
@@ -21,6 +32,7 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        logger.info("Chromedriver created");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
     }
